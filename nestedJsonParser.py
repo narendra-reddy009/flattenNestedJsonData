5c81ef3b1bee35b90e86b2479611f506f2b04ec2
Narendra Reddy Dasireddigari <narendra.reddy009@yahoo.com>
2021-07-02 11:11:16 +0530

    initial code development

--- nestedJsonParser.py
+++ nestedJsonParser.py
@@ -1,20 +1,23 @@
+##This Function is developed for flattening the very nested Json Data/Record into a Dataframe or Equivalent to Spreadsheet type.
+
 def flattenNestedData(nestedDF):
-   fieldNames = dict([(field.name, field.dataType) for field in nestedDF.schema.fields if type(field.dataType) == StructType]) ##Add for Array --> type(field.dataType) == ArrayType or
-   ##Fetching Complex Datatype Columns from Schema
+     ##Fetching Complex Datatype Columns from Schema
+   fieldNames = dict([(field.name, field.dataType) for field in nestedDF.schema.fields if type(field.dataType) == StructType]) ##Add if you want to explode ArrayType --> type(field.dataType) == ArrayType or
+
    while len(fieldNames)!=0:
       fieldName=list(fieldNames.keys())[0]
       print ("Processing :"+fieldName+" Type : "+str(type(fieldNames[fieldName])))
 
-      if (type(fieldNames[fieldName]) == StructType):
+      if type(fieldNames[fieldName]) == StructType:
          extractedFields = [col(fieldName +'.'+ innerColName).alias(innerColName) for innerColName in [ colName.name for colName in fieldNames[fieldName]]]
          nestedDF=nestedDF.select("*", *extractedFields).drop(fieldName)
 
-      elif (type(fieldNames[fieldName]) == ArrayType):
+      elif type(fieldNames[fieldName]) == ArrayType: ##If we enable the ArrayType in Line 2 & 15, we end up having multiple duplicate records. Each array column value will create new record, which is worst.
          nestedDF=nestedDF.withColumn(fieldName,explode_outer(fieldName))
 
-      fieldNames = dict([(field.name, field.dataType) for field in nestedDF.schema.fields if type(field.dataType) == StructType]) ##Add for Array --> type(field.dataType) == ArrayType or
+      fieldNames = dict([(field.name, field.dataType) for field in nestedDF.schema.fields if type(field.dataType) == StructType]) ##Add if you want to explode ArrayType --> type(field.dataType) == ArrayType or
    return nestedDF
 
-finalDF = flattenNestedData(nestedDF)
-display(finalDF)
-finalDF.show()
+finalData = flattenNestedData(nestedDF) ## Dataframe you wanted to flatten it up.na
+display(finalData)
+finalData.show()
